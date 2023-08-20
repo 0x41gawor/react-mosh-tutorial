d203ed9ebc709f0c5273c7c2105bc252fbe5ef2f
0x41gawor <41gawor@gmail.com>
2023-08-20 14:21:37 +0000

    Rendered a list

--- README.md
+++ README.md
@@ -42,3 +42,29 @@ function App() {
 export default App;
 ```
 
+
+## Render a list
+
+```typescript
+function ListGroup() {
+  const items = ["New York", "San Francisco", "Paris", "Berlin", "Tokyo"];
+
+  // In real word you will use item.id as a key
+  // Niesamowicie mi sie podoba ten syntax: metoda map na liscie zmapowala jej kazdy element na element html
+  // pamietaj ze JFX jest translowany na html dlatego kod trzeba wrapnac w curly brackets
+  // ten syntax z <> i </> ktory wrapuje caly kod mowi do React'a ze to ma byc zamienione tylko na jedna linijke React.createElement('...')
+  return (
+    <>
+      <h1>List</h1>
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
+```
+
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,19 @@
 function ListGroup() {
+  const items = ["New York", "San Francisco", "Paris", "Berlin", "Tokyo"];
+
+  // In real word you will use item.id as a key
+  // Niesamowicie mi sie podoba ten syntax: metoda map na liscie zmapowala jej kazdy element na element html
+  // pamietaj ze JFX jest translowany na html dlatego kod trzeba wrapnac w curly brackets
+  // ten syntax z <> i </> ktory wrapuje caly kod mowi do React'a ze to ma byc zamienione tylko na jedna linijke React.createElement('...')
   return (
-    <ul className="list-group">
-      <li className="list-group-item">Cras justo odio</li>
-      <li className="list-group-item">Dapibus ac facilisis in</li>
-      <li className="list-group-item">Morbi leo risus</li>
-      <li className="list-group-item">Porta ac consectetur ac</li>
-      <li className="list-group-item">Vestibulum at eros</li>
-    </ul>
+    <>
+      <h1>List</h1>
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
   );
 }
 
